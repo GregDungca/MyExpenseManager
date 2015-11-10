c9aa78ab68d4726344f642306dff1e231f0b0782
GregDungca <greg.dungca@gmail.com>
2015-11-09 21:58:57 -0800

    Working on form

--- client/app/expenses/expenses.js
+++ client/app/expenses/expenses.js
@@ -1,5 +1,8 @@
 var expensesApp = angular.module('expensesApp', []);// what does this do exactly?
 
 expensesApp.controller('ExpensesCtrl', function ($scope) {
-  
+
+  var submitExpense = function () {
+    // call the associated handler within services.js
+  }
 });
